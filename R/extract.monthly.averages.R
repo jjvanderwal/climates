94000e8d270590c59d410db2b19f9f93832ed070
Jeremy VanDerWal <jjvanderwal@gmail.com>
2010-05-30 23:06:42 +0000

    correcting & testing portions of package

--- R/extract.monthly.averages.R
+++ R/extract.monthly.averages.R
@@ -9,13 +9,13 @@ extract.monthly.averages <- function(nc,min.year,max.year) {
 	lon = nc$lon
 	mm = 1:12
 	#create an output array
-	out.data = array(0,dim=c(length(mm),length(lat),length(lon)))
+	out = array(0,dim=c(length(mm),length(lat),length(lon)))
 	#get the array subset which is from the start year to end year
 	years = which(nc$yy %in% min.year:max.year)
 	#cycle through all the data in those years
-	for (ii in years) out.data[nc$mm[ii],,] = out.data[nc$mm[ii],,] + nc$dat[ii,,]
+	for (ii in years) out[nc$mm[ii],,] = out[nc$mm[ii],,] + nc$dat[ii,,]
 	#now get the average by deviding by the number of months
-	for (ii in mm) out.data[ii,,] = out.data[ii,,] / (max.year-min.year+1)
+	for (ii in mm) out[ii,,] = out[ii,,] / (max.year-min.year+1)
 	#return the information
 	out = list(dat=out,lon=nc$lon,lat=nc$lat,tim=nc$tim)
 	class(out) = unique(c(class(out),'nc'))
