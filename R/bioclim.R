8fff300bc061d3a23ab73cfb04b4f313d9ae04c4
Jeremy VanDerWal <jjvanderwal@gmail.com>
2010-06-04 06:37:26 +0000

    corrected bioclim.R

--- R/bioclim.R
+++ R/bioclim.R
@@ -13,7 +13,7 @@ function(tmin=NULL, tmax=NULL, prec=NULL, tmean=NULL, vois=1:19, cov=FALSE, t.as
 			stop('tmin is needed for the variables selected')
 		} else if (is.data.frame(tmin) | is.matrix(tmin)) { #check if tmin is a dataframe or matrix
 			tmin = as.matrix(tmin) #convert it to a matrix
-			if (dim(tmin)[2] %in% c(12,52)) stop('tmin must have 12 columns -- one for each month')	
+			if (!dim(tmin)[2] %in% c(12,52)) stop('tmin must have 12 columns -- one for each month')	
 			tsize = c(tsize,dim(tmin)[1])
 		} else { stop('tmin must be a data.frame or matrix') }
 	}
@@ -22,7 +22,7 @@ function(tmin=NULL, tmax=NULL, prec=NULL, tmean=NULL, vois=1:19, cov=FALSE, t.as
 			stop('tmax is needed for the variables selected')
 		} else if (is.data.frame(tmax) | is.matrix(tmax)) { #check if tmax is a dataframe or matrix
 			tmax = as.matrix(tmax) #convert it to a matrix
-			if (dim(tmax)[2] %in% c(12,52)) stop('tmax must have 12 columns -- one for each month')	
+			if (!dim(tmax)[2] %in% c(12,52)) stop('tmax must have 12 columns -- one for each month')	
 			tsize = c(tsize,dim(tmax)[1])
 		} else { stop('tmax must be a data.frame or matrix') }	
 	}
@@ -31,7 +31,7 @@ function(tmin=NULL, tmax=NULL, prec=NULL, tmean=NULL, vois=1:19, cov=FALSE, t.as
 			stop('prec is needed for the variables selected')
 		} else if (is.data.frame(prec) | is.matrix(prec)) { #check if prec is a dataframe or matrix
 			prec = as.matrix(prec) #convert it to a matrix
-			if (dim(prec)[2] %in% c(12,52)) stop('prec must have 12 columns -- one for each month')	
+			if (!dim(prec)[2] %in% c(12,52)) stop('prec must have 12 columns -- one for each month')	
 			tsize = c(tsize,dim(prec)[1])
 		} else { stop('prec must be a data.frame or matrix') }
 	}
@@ -42,7 +42,7 @@ function(tmin=NULL, tmax=NULL, prec=NULL, tmean=NULL, vois=1:19, cov=FALSE, t.as
 			tmean = (tmax+tmin)/2 
 		} else if (is.data.frame(tmean) | is.matrix(tmean)) { #check if tmean is a dataframe or matrix
 			tmean = as.matrix(tmean) #convert it to a matrix
-			if (dim(tmean)[2] %in% c(12,52)) stop('tmean must have 12 columns -- one for each month')
+			if (!dim(tmean)[2] %in% c(12,52)) stop('tmean must have 12 columns -- one for each month')
 			tsize = c(tsize,dim(tmean)[1])			
 		} else { stop('tmean must be null, a data.frame or a matrix') }
 	}
@@ -59,11 +59,8 @@ function(tmin=NULL, tmax=NULL, prec=NULL, tmean=NULL, vois=1:19, cov=FALSE, t.as
 	if (any(vois == 2)) { out[,2] = rowMeans(tmax-tmin,na.rm=T) }
 	#temperature seasonality --4
 	if (any(vois == 4)) {
-		if (cov) { #calculate it as anuclim does (cov)
-			out[,4] = (apply(tmean,1,function(x) { return(sd(x,na.rm=T)) }) / (out[,1]+273.15)) * 100
-		} else { #calculate it as worldclim does (sd)
-			out[,4] = apply(tmean,1,function(x) { return(sd(x,na.rm=T)) })
-		}
+		out[,4] = apply(tmean,1,function(x) { return(sd(x,na.rm=T)) }) #this is as worldclim
+		if (cov) { out[,4] = out[,4] / (rowMeans(tmean,na.rm=T)+273.15) * 100 } #calculate it as anuclim does (cov)
 	}
 	#max temperature of the warmest period -- 5
 	if (any(vois %in% c(5,3,7))) { out[,5] = apply(tmax,1,function(x) { return(max(x,na.rm=T)) }) }
@@ -74,11 +71,11 @@ function(tmin=NULL, tmax=NULL, prec=NULL, tmean=NULL, vois=1:19, cov=FALSE, t.as
 	#isothermality (2/7) -- 3
 	if (any(vois == 3)) { out[,3] = out[,2] / out[,7] }
 	#annual precipitation -- 12
-	if (any(vois == 12)) { out[,12] == rowSums(prec,na.rm=T) }
+	if (any(vois == 12)) { out[,12] = rowSums(prec,na.rm=T) }
 	#precipitation of the wettest period -- 13
-	if (any(vois == 13)) { out[,13] == apply(prec,1,function(x) { return(max(x,na.rm=T)) }) }
+	if (any(vois == 13)) { out[,13] = apply(prec,1,function(x) { return(max(x,na.rm=T)) }) }
 	#precipitation of the driest period -- 14
-	if (any(vois == 14)) { out[,14] == apply(prec,1,function(x) { return(min(x,na.rm=T)) }) }
+	if (any(vois == 14)) { out[,14] = apply(prec,1,function(x) { return(min(x,na.rm=T)) }) }
 	#precipitation seasonality (cov) -- 15
 	if (any(vois == 15)) { out[,15] = apply(prec,1,function(x) { return(sd(x,na.rm=T)) }) / rowMeans(prec,na.rm=T) }
 	#work with warmest / coolest quarters
@@ -109,12 +106,12 @@ function(tmin=NULL, tmax=NULL, prec=NULL, tmean=NULL, vois=1:19, cov=FALSE, t.as
 			}
 			#precipitation of the warmest quarter -- 18
 			if (any(vois == 18)) { 
-				tt = NULL; for (ii in 1:nrow(tt2)) { tt = c(tt,which(tt1[ii,]==out[ii,10])) }
+				tt = rep(0,nrow(tt2)); for (ii in 1:nrow(tt2)) { tt[ii] = which(tt1[ii,]==out[ii,10])[1] }
 				out[,18] = tt2[cbind(1:nrow(tt2),tt)]
 			}
 			#precipitation of the coolest quarter -- 19
 			if (any(vois == 19)) { 
-				tt = NULL; for (ii in 1:nrow(tt2)) { tt = c(tt,which(tt1[ii,]==out[ii,11])) }
+				tt = rep(0,nrow(tt2)); for (ii in 1:nrow(tt2)) { tt[ii] = which(tt1[ii,]==out[ii,11])[1] }
 				out[,19] = tt2[cbind(1:nrow(tt2),tt)]
 			}
 			rm(tt2);rm(tt)
@@ -147,22 +144,31 @@ function(tmin=NULL, tmax=NULL, prec=NULL, tmean=NULL, vois=1:19, cov=FALSE, t.as
 				(ii+3)%%52+1),(ii+4)%%52+1,(ii+5)%%52+1,(ii+6)%%52+1,
 				(ii+7)%%52+1,(ii+8)%%52+1,(ii+9)%%52+1,(ii+10)%%52+1,(ii+11)%%52+1],na.rm=T) }
 			}
-			#mean temperature of the wettest quarter -- 10
-			if (any(vois == 18)) { 
-				tt = NULL; for (ii in 1:nrow(tt2)) { tt = c(tt,which(tt1[ii,]==out[ii,16])) }
-				out[,18] = tt2[cbind(1:nrow(tt2),tt)]
+			#mean temperature of the wettest quarter -- 8
+			if (any(vois == 8)) { 
+				tt = rep(0,nrow(tt2)); for (ii in 1:nrow(tt2)) { tt[ii] = which(tt1[ii,]==out[ii,16])[1] }
+				out[,8] = tt2[cbind(1:nrow(tt2),tt)]
 			}
-			#mean temperature of the driest quarter -- 11
-			if (any(vois == 19)) { 
-				tt = NULL; for (ii in 1:nrow(tt2)) { tt = c(tt,which(tt1[ii,]==out[ii,17])) }
-				out[,18] = tt2[cbind(1:nrow(tt2),tt)]
+			#mean temperature of the driest quarter -- 9
+			if (any(vois == 9)) { 
+				tt = rep(0,nrow(tt2)); for (ii in 1:nrow(tt2)) { tt[ii] = which(tt1[ii,]==out[ii,17])[1] }
+				out[,9] = tt2[cbind(1:nrow(tt2),tt)]
 			}
 			rm(tt2);rm(tt)
 		}
 		rm(tt1)
 	}
+	#do the necessary rounding
+	if (t.as.int) {
+		out[,c(1:3,5:11)] = round(out[,c(1:3,5:11)]*10)
+	} else {
+		out[,c(1:3,5:11)] = round(out[,c(1:3,5:11)],1)
+	}
+	out[,c(4,15)] = round(out[,c(4,15)]*100)
+	out[,c(12:14,16:19)] = round(out[,c(12:14,16:19)])
+	
 	#define the colnames
-	colnames(out) = paste('bioclim',1:19,sep='_')
+	colnames(out) = paste('bioclim',sprintf('%02i',1:19),sep='_')
 	
 	#return the output
 	return(out[,vois])
