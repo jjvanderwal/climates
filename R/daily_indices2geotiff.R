2aa8d04e25231f545113c72bfcaeda5211c52804
David Blodgett <dblodgett@usgs.gov>
2014-11-28 09:25:16 -0600

    Moved some code to the climates package... there should be a way around doing this at all.

--- R/daily_indices2geotiff.R
+++ R/daily_indices2geotiff.R
@@ -1,5 +1,9 @@
 daily_indices2geotiff<-function(tmax_data,tmin_data,prcp_data,tave_data,thresholds,coords_master,prj,year,time_PCICt)
 {
+  tmax_data<-t(tmax_data)
+  tmin_data<-t(tmin_data)
+  prcp_data<-t(prcp_data)
+  tave_data<-t(tave_data)
   #remove cells that are NaNs.
   mask<-!is.na(prcp_data[,1])
   masked_coords<-coords_master[mask,]
